d718709a4f836d0238316e3ceed4d2214e18c6f3
Matthew Lim <personal.matthewjlim@gmail.com>
2023-03-12 20:18:12 -0700

    extensive work on flask web app

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from spotipy.oauth2 import SpotifyOAuth
 import time
 import pickle
 import numpy as np
+import pandas as pd
+from pprint import pprint
+from playlist import Playlist
 
 app = Flask(__name__)
 
@@ -13,7 +16,7 @@ app.secret_key = "2yf8d9asDF"
 app.config['SESSION_COOKIE_NAME'] = 'Matthews Cookie'
 TOKEN_INFO = 'token_info'
 
-@app.route('/')
+@app.route('/login')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
@@ -27,9 +30,9 @@ def redirectPage():
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session[TOKEN_INFO] = token_info
-    return redirect(url_for('index'))
+    return redirect(url_for('dashboard'))
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -52,16 +55,84 @@ def getTracks():
         if(len(items) < 50):
             break
     return str(len(all_songs))
-    return 0
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
-    features = [np.array(int_features)]  
-    prediction = model.predict(features) 
-    result = prediction[0]
+    try:
+            token_info = get_token()
+    except:
+        print('User Not Logged In!')
+        return redirect('/')
+    # at this part, we ensured the token info is up-to-date / fresh
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+    playlist_id = [str(x) for x in request.form.values()][0]
+    creator = sp.current_user
+    print('\nSelected Playlist ID:\n')
+    print(playlist_id)
 
-    return render_template('index.html', prediction=result)
+    def analyze_playlist(creator, playlist_id):
+        # Create empty dataframe
+        playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
+        
+        playlist_df = pd.DataFrame(columns = playlist_features_list)
+        
+        # Loop through every track in the playlist, extract features and append the features to the playlist df
+        playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
+        for track in playlist:
+            # Create empty dict
+            playlist_features = {}
+            # Get metadata
+            playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+            playlist_features["album"] = track["track"]["album"]["name"]
+            playlist_features["track_name"] = track["track"]["name"]
+            playlist_features["track_id"] = track["track"]["id"]
+            
+            # Get audio features
+            audio_features = sp.audio_features(playlist_features["track_id"])[0]
+            for feature in playlist_features_list[4:]:
+                playlist_features[feature] = audio_features[feature]
+            
+            # Concat the dfs
+            track_df = pd.DataFrame(playlist_features, index = [0])
+            playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+            
+        return playlist_df
+    
+    def classify_playlist(creator, playlist_id):
+        playlist = analyze_playlist(creator, playlist_id)
+        playlist_predictions = model.predict(playlist.drop(['artist','album','track_name','track_id'],axis=1))
+        playlist['prediction'] = playlist_predictions
+        playlist['prediction'] = playlist['prediction'].replace(1, 'Happy')
+        playlist['prediction'] = playlist['prediction'].replace(2, 'Sad')
+        return playlist
+    
+    predictions = classify_playlist(creator, playlist_id)
+    happy_predictions = predictions[predictions['prediction'] == 'Happy']
+    sad_predictions = predictions[predictions['prediction'] == 'Sad']
+
+    return render_template('predict.html',tables=[happy_predictions.to_html(classes='data'),sad_predictions.to_html(classes='data')], titles=[happy_predictions.columns.values,sad_predictions.columns.values])
+
+@app.route('/dashboard')
+def dashboard():
+    try:
+        token_info = get_token()
+    except:
+        print('User Not Logged In!')
+        return redirect('/')
+    # at this part, we ensured the token info is up-to-date / fresh
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    playlists = []
+    iter = 0
+    while True:
+        items = sp.current_user_playlists(limit=50,offset=iter * 50)['items']
+        iter += 1
+        playlists += items
+        if(len(items) < 50):
+            break
+    pprint(playlists)
+    return render_template('dashboard.html',data=playlists) # data is list
     
 
 def get_token():
