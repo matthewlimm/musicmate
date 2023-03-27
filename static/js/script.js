cce342f295295048c4349d1ff40ea82e24328892
Matthew Lim <personal.matthewjlim@gmail.com>
2023-03-27 12:54:47 -0700

    fixed dynamic pause-play icons

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function pausePlay(input) {
+    console.log(input.tagName);
+    var playButton = input.parentElement.querySelector('span');
+    pauseButtons = document.getElementsByClassName('fa fa-pause')
+    for(i=0;i<pauseButtons.length;i++)
+    {
+        pauseButtons[i].className = 'fa fa-play';
+    }
+    if (input.checked || input.tagName == 'FORM')
+    {
+        playButton.className = "fa fa-pause";
+    } 
+    else 
+    {
+        playButton.className = "fa fa-play";
+    }
+}
