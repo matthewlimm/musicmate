abb831264fd78da5be7342c4aad1a8a71ddc2e5c
Matthew Lim <personal.matthewjlim@gmail.com>
2023-04-02 02:08:35 -0700

    predict page
    - colored borders to indicate track prediction
    - generate buttons

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ def dashboard():
         return redirect('/')
     # at this part, we ensured the token info is up-to-date / fresh
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    creator = sp.user(sp.me()['id'])
-    return render_template('dashboard.html',data=creator)
+    user = sp.user(sp.me()['id'])
+    return render_template('dashboard.html',user=user)
 
 @app.route('/happyvsad')
 def happyvsad():
@@ -79,7 +79,7 @@ def happyvsad():
         return redirect('/')
     # at this part, we ensured the token info is up-to-date / fresh
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    creator = sp.user(sp.me()['id'])
+    user = sp.user(sp.me()['id'])
 
     playlists = []
     iter = 0
@@ -90,7 +90,7 @@ def happyvsad():
         if(len(items) < 50):
             break
     pprint(playlists)
-    return render_template('happyvsad.html',data=creator,data2=playlists) # data is list
+    return render_template('happyvsad.html',user=user,playlists=playlists) # data is list
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -102,18 +102,18 @@ def predict():
     # at this part, we ensured the token info is up-to-date / fresh
     sp = spotipy.Spotify(auth=token_info['access_token'])
     
-    creator = sp.current_user
+    user = sp.current_user
     playlist_id = [str(x) for x in request.form.values()][0]
     print('\nSelected Playlist ID: ' + playlist_id)
 
-    def analyze_playlist(creator, playlist_id):
+    def analyze_playlist(user, playlist_id):
         # Create empty dataframe
-        playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
+        playlist_features_list = ["artist","album","track_name", "track_id","track_image_url","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
         
         playlist_df = pd.DataFrame(columns=playlist_features_list)
         
         # Loop through every track in the playlist, extract features and append the features to the playlist df
-        playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
+        playlist = sp.user_playlist_tracks(user, playlist_id)["items"]
         for track in playlist:
             # Create empty dict
             playlist_features = {}
@@ -122,10 +122,11 @@ def predict():
             playlist_features["album"] = track["track"]["album"]["name"]
             playlist_features["track_name"] = track["track"]["name"]
             playlist_features["track_id"] = track["track"]["id"]
+            playlist_features["track_image_url"] = sp.track(track["track"]["id"])['album']['images'][0]['url']
             
             # Get audio features
             audio_features = sp.audio_features(playlist_features["track_id"])[0]
-            for feature in playlist_features_list[4:]:
+            for feature in playlist_features_list[5:]:
                 playlist_features[feature] = audio_features[feature]
             
             # Concat the dfs
@@ -134,21 +135,23 @@ def predict():
             
         return playlist_df
     
-    def classify_playlist(creator, playlist_id):
-        playlist = analyze_playlist(creator, playlist_id)
-        playlist_predictions = model.predict(playlist.drop(['artist','album','track_name','track_id'],axis=1))
+    def classify_playlist(user, playlist_id):
+        playlist = analyze_playlist(user, playlist_id)
+        playlist_predictions = model.predict(playlist.drop(['artist','album','track_name','track_id','track_image_url'],axis=1))
         playlist['prediction'] = playlist_predictions
         playlist['prediction'] = playlist['prediction'].replace(1, 'Happy')
         playlist['prediction'] = playlist['prediction'].replace(2, 'Sad')
         return playlist
     
-    predictions = classify_playlist(creator, playlist_id)
+    predictions = classify_playlist(user, playlist_id)
+    print(predictions)
     happy_predictions = predictions[predictions['prediction'] == 'Happy']
     sad_predictions = predictions[predictions['prediction'] == 'Sad']
 
     #track_ids = [predictions['track_id'] for predictions in predictions]
     #session['track_ids'] = track_ids
-    return render_template('predict.html',tables=[happy_predictions.to_html(index=False,classes='data'),sad_predictions.to_html(index=False,classes='data')], titles=[happy_predictions.columns.values,sad_predictions.columns.values],data=predictions)
+    user = sp.user(sp.me()['id'])
+    return render_template('predict_new.html',columns=[happy_predictions.columns.values,sad_predictions.columns.values], rows=[list(happy_predictions.values.tolist()),list(sad_predictions.values.tolist())],user=user)
     
 @app.route("/savePlaylist", methods=['GET', 'POST'])
 def savePlaylist():
@@ -160,27 +163,28 @@ def savePlaylist():
     # at this part, we ensured the token info is up-to-date / fresh
     sp = spotipy.Spotify(auth=token_info['access_token'])
 
-    track_id = [str(x) for x in request.form.values()][0]
-    track_id = pd.read_html(track_id)[0]
-    if track_id['prediction'][0] == 'Happy':
+    prediction_table = [str(x) for x in request.form.values()][0]
+    print(prediction_table)
+    prediction_table = pd.read_html(prediction_table)[0]
+    if prediction_table['prediction'][0] == 'Happy':
         prediction = 'Happy'
     else:
         prediction = 'Sad'
-    track_id.drop(['artist','album','track_name','danceability','energy','key','loudness','mode','speechiness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature','prediction'],axis=1,inplace=True)
-    track_id = track_id.values.tolist()
-    track_id = list(np.concatenate(track_id).flat)
+    track_ids = prediction_table.drop(['artist','album','track_name','danceability','energy','key','loudness','mode','speechiness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature','prediction'],axis=1)
+    track_ids = track_ids.values.tolist()
+    track_ids = list(np.concatenate(track_ids).flat)
     
-    creator = sp.me()['id']
+    user = sp.me()['id']
     if prediction == 'Happy':
-        playlist_name = "My Purified (Happy) Playlist"
+        playlist_name = "My Happy Playlist"
     else:
-        playlist_name = "My Purified (Sad) Playlist"
+        playlist_name = "My Sad Playlist"
 
-    sp.user_playlist_create(user=creator,name=playlist_name,public=False,description="Generated by Matthew Lim.")
+    sp.user_playlist_create(user=user,name=playlist_name,public=False,description="Generated by Matthew Lim.")
 
-    def getPlaylistID(creator, playlist_name):
+    def getPlaylistID(user, playlist_name):
         playlist_id = ''
-        playlists = sp.user_playlists(creator)
+        playlists = sp.user_playlists(user)
         for playlist in playlists['items']:  # iterate through playlists I follow
             if playlist['name'] == playlist_name:  # filter for newly created playlist
                 playlist_id = playlist['id']
@@ -188,10 +192,10 @@ def savePlaylist():
         return playlist_id
     
     print('Track IDs to Pass Through Spotify API')
-    pprint(track_id)
+    pprint(track_ids)
 
-    playlist_id = getPlaylistID(creator, playlist_name)
-    sp.user_playlist_add_tracks(creator, playlist_id, track_id)
+    playlist_id = getPlaylistID(user, playlist_name)
+    sp.user_playlist_add_tracks(user, playlist_id, track_ids)
     return 'Success'
 
 def get_token():
